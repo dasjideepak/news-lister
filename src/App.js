bde97569b0723d9fc9426ea02a8dd4569bd30fc4
dasjideepak <dasjideepak@gmail.com>
2022-08-26 18:53:15 +0530

    feat: fetch data and pagination

--- src/App.js
+++ src/App.js
@@ -1,23 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import ReactPaginate from "react-paginate";
+import "./App.css";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [page, setPage] = useState(1);
+
+  const [searchText, setSearchText] = useState("");
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    if (searchText) {
+      const URL = `http://content.guardianapis.com/search?api-key=test&amp;q=${searchText}&amp;show-fields=thumbnail,headline&amp;show-tags=keyword&amp;page=${page}&amp;page-size=10`;
+      setIsLoading(true);
+      fetch(URL).then((res) =>
+        res
+          .json()
+          .then((data) => {
+            data = data?.response;
+            setData(data);
+          })
+          .finally(() => setIsLoading(false))
+      );
+    }
+
+    setData({ currentPage: 1, results: [] });
+  }, [searchText, page]);
+
+  const handlePageClick = (event) => {
+    setPage(event.selected + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex-center">
+      <h1>News Lister</h1>
+      <div className="search">
+        <label htmlFor="search">Enter Search Text</label>
+        <input
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+          name="Search"
+          id="search"
+        />
+      </div>
+      {isLoading ? (
+        <div className="flex-center loader">
+          <span>Loading...</span>
+        </div>
+      ) : data?.results?.length ? (
+        <div>
+          <div className="flex justify-between">
+            <h2>Results for {searchText}</h2>
+            <div>
+              <span>Showing {data?.pageSize} Items</span>
+              <h3>Total {data?.total} Items</h3>
+            </div>
+          </div>
+
+          {data?.results?.map((item) => {
+            return (
+              <div key={item?.id} className="card">
+                <a href={item?.webUrl}>
+                  <div className="flex-center">
+                    <img
+                      width={100}
+                      height={100}
+                      src={
+                        item?.thumbnail ||
+                        "https://t4.ftcdn.net/jpg/02/07/87/79/360_F_207877921_BtG6ZKAVvtLyc5GWpBNEIlIxsffTtWkv.jpg"
+                      }
+                      alt={item?.pillarName}
+                    />
+                  </div>
+                </a>
+                <div className="title-and-keywords">
+                  <a href={item?.webUrl}>{item?.webTitle}</a>
+                </div>
+              </div>
+            );
+          })}
+          <ReactPaginate
+            breakLabel="..."
+            nextLabel="next >"
+            onPageChange={handlePageClick}
+            pageRangeDisplayed={10}
+            pageCount={data?.pages}
+            previousLabel="< prev"
+            renderOnZeroPageCount={null}
+            className="flex-center pagination"
+          />
+        </div>
+      ) : null}
     </div>
   );
 }
